e9fa080813d19012b4684b161f52a907306d9b4b
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-03-02 14:53:25 +0100

    mogus

---
+++ src/stage/week1.c
@@ -0,0 +1,82 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include "week1.h"
+
+#include "../archive.h"
+#include "../mem.h"
+#include "../stage.h"
+
+int bgx = 69;
+
+//Week 1 background structure
+typedef struct
+{
+	//Stage background base structure
+	StageBack back;
+	
+	//Textures
+	Gfx_Tex tex_back0; //Stage and back
+
+} Back_Week1;
+
+//Week 1 background functions
+void Back_Week1_DrawBG(StageBack *back)
+{
+	Back_Week1 *this = (Back_Week1*)back;
+	
+	fixed_t fx, fy;
+	
+	//Draw curtains
+	fx = stage.camera.x;
+	fy = stage.camera.y;
+
+	RECT bg_src = {122, 0, 134, 256};
+	RECT_FIXED bg_dst = {
+		FIXED_DEC(bgx,1) - fx,
+		FIXED_DEC(-150,1) - fy,
+		FIXED_DEC(134,1),
+		FIXED_DEC(256,1)
+	};
+	
+	Stage_DrawTex(&this->tex_back0, &bg_src, &bg_dst, stage.camera.bzoom);
+
+	//move da funni
+	bgx -= 10;
+
+	if (bgx <= -50)
+		bgx = 69;
+
+}
+
+void Back_Week1_Free(StageBack *back)
+{
+	Back_Week1 *this = (Back_Week1*)back;
+	
+	//Free structure
+	Mem_Free(this);
+}
+
+StageBack *Back_Week1_New(void)
+{
+	//Allocate background structure
+	Back_Week1 *this = (Back_Week1*)Mem_Alloc(sizeof(Back_Week1));
+	if (this == NULL)
+		return NULL;
+	
+	//Set background functions
+	this->back.draw_fg = NULL;
+	this->back.draw_md = NULL;
+	this->back.draw_bg = Back_Week1_DrawBG;
+	this->back.free = Back_Week1_Free;
+	
+	//Load background textures
+	IO_Data arc_back = IO_Read("\\WEEK1\\BACK.ARC;1");
+	Gfx_LoadTex(&this->tex_back0, Archive_Find(arc_back, "back0.tim"), 0);
+	Mem_Free(arc_back);
+	
+	return (StageBack*)this;
+}
